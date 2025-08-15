fb8e33c0ecd03c17418cee54b1d004bdf1ff9a79
Shivendra Tripathi <shivendraking19@gmail.com>
2025-08-16 01:09:17 +0530

    correct the cors problem

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://petadoptionsys-1.onrender.com/",
+    origin: "https://petadoptionsys-1.onrender.com",
     credentials: true,
   })
 );
